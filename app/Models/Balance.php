2977e12fd01a8e949b8e38fb4688a90a70ee5bee
LucasMobitap <lucassms9@hotmail.com>
2018-07-29 14:37:36 -0300

    registro de historico
    registro de historico e feito o relacionamento hasmany na model user

--- app/Models/Balance.php
+++ app/Models/Balance.php
@@ -11,10 +11,21 @@ class Balance extends Model
         public $timestamps = false;
         public function deposit(float $value) :Array
         {
-            $this->amount += number_format($value, 2, '.', '') ; 
+
+            $total_before = $this->amount ? $this->amount : 0 ;
+            $this->amount += number_format($value, 2, '.', '') ;
+            $total_after = $this->amount;         
             $deposit = $this->save();
 
-            if($deposit){
+            $historic = auth()->user()->historics()->create([
+                'type'          => 'I',
+                'amount'        => $value,
+                'total_before'  => $total_before ,
+                'total_after'   => $total_after,
+                'date'          => date('Ymd'),
+            
+            ]);
+            if($deposit && $historic){
                 return [
                     'success' => true,
                     'message' => 'Sucesso ao recarregar'
